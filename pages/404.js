068b957ae16d95c6b07ddd70c92f1153174efed5
Alejandro Pérez <72344440+alejandroperez01@users.noreply.github.com>
2023-01-13 18:40:16 +0000

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -9,7 +9,7 @@ export default function Home() {
             <title>Error 404</title>
             <h3>Page not found</h3>
             <h2>But you can drink something with the camel brothers &#128526;</h2>
-            <img src = "/camel.jpg" className={styles.main} />
+            <img src = "/camel.jfif" className={styles.main} />
         </main>
     </div>
     </>
