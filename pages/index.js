a29471631ee25be1e2430e8441d151444242e3a5
Alejandro Pérez <72344440+alejandroperez01@users.noreply.github.com>
2023-01-13 18:38:02 +0000

    update

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,13 @@ import styles from "./index.module.css";
 import styled from "styled-components";
 
 const GreenButton = styled.button`
-background-color: green;
+background-color: #00cc99;
 color: white;
 font-size: 20px;
 padding: 10px 20px;
-border-color: green;
-border-radius: 5px;
-margin: 10px 0px;
+border-color: white;
+border-radius: 10px;
+margin: 10px 10px;
 cursor: pointer;
 `;
 const RedButton = styled.button`
@@ -18,8 +18,8 @@ background-color: red;
 color: white;
 font-size: 20px;
 padding: 10px 20px;
-border-color: red;
-border-radius: 5px;
+border-color: white;
+border-radius: 10px;
 margin: 10px 0px;
 cursor: pointer;
 `;
@@ -49,7 +49,7 @@ return (
       </Link>
       <br></br><br></br>
       <h2><img src="/404.png" className={styles.icon} /></h2>
-      <h2>Error 404 (why would you want to go here??</h2>
+      <h2>Error 404 (why would you want to go here??)</h2>
       <Link href="/404">
         <RedButton>404</RedButton>
       </Link>
